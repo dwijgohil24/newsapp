d7f59add5a9db1e4d9e616a16af3a60caefd14fa
dwijgohil <20ecuot047@ddu.ac.in>
2023-05-24 08:50:58 +0530

    "poppulated articles through news-api"

--- src/Components/NewItem.js
+++ src/Components/NewItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewItem extends Component {
+
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
+      <div>                                                    
         <div className="card" style={{width: "18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src={imageUrl?imageUrl : "https://cdn.ebaumsworld.com/mediaFiles/picture/2452130/85386506.jpg"} height="256px" width="256px" className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="#" className="btn btn-primary">View-More</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}....</p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">View-More</a>
         </div>
         </div>
       </div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -2,24 +2,40 @@ import React, { Component } from 'react'
 import NewItem from './NewItem'
 
 export class News extends Component {
+    constructor(){
+        super();
+        console.log("New object of news is created !");
+        this.state = {
+            articles : [],
+            loading : false,
+        }
+    }
+  async componentDidMount(){
+
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=62a08eda3a134c90a82aa8f3741ddaf2";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles: parsedData.articles})
+    
+  }
   render() {
     return (
       <div className="container my-3">
 
-        <h2>News-Monkey Top Headlines ! </h2>
+        <h2>News-Monkey Top Headlines !</h2>
 
         <div className="row">
-            <div className="col-md-4">
-                <NewItem title="my title" description="my description"/>
-            </div>
-            <div className="col-md-4">
-                <NewItem title="my title" description="my description"/>
-            </div>
-            <div className="col-md-4">
-                <NewItem title="my title" description="my description"/>
-            </div>
+
+        {this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url}>
+                
+                      <NewItem title={element.title?element.title.slice(0,45) : ""} description={element.description?element.description.slice(0,88):""} imageUrl = {element.urlToImage?element.urlToImage:""} newsUrl = {element.url?element.url:""}/>
+                    
+                   </div>
+            })}
+
         </div>
-        
+
       </div>
     )
   }
